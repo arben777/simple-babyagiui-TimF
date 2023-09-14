82594eb131e2d6b9f905a623e4cac43b4f0612ba
arben777 <arbengb@gmail.com>
2023-09-13 20:34:31 -0700

    bulelt points examples

--- main.py
+++ main.py
@@ -270,12 +270,12 @@ def main():
             investor_prompts = investor_specific_prompts.get(DEFAULT_INVESTOR, {"objectives": [], "tasks": []})
 
             OBJECTIVE = st.text_input(
-                label=f"🏁 :orange[What's Your Ultimate Goal]:\n" + "\n".join(f"{i+1}. {objective}" for i, objective in enumerate(investor_prompts['objectives'])),
+                label=f"🏁 :orange[What's Your Ultimate Goal]:\n" + "\n".join(f"• {i+1}. {objective}" for i, objective in enumerate(investor_prompts['objectives'].split(','))),
                 value="Type your own goal here!",
             )
 
             first_task = st.text_input(
-                label=f"🥇:range[Initial task:]\n" + "\n".join(f"{i+1}. {task}" for i, task in enumerate(investor_prompts['tasks'])),
+                label=f"🥇:range[Initial task:]\n" + "\n".join(f"• {i+1}. {task}" for i, task in enumerate(investor_prompts['tasks'].split(','))),
                 value="Type your own task here!",
             )
 
